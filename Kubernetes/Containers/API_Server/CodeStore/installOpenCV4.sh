cac822f6528398e7d32edea1aec107b89da4ab82
pgoode41 <pgoode41@gmail.com>
2019-11-17 20:53:20 -0600

    Update

--- Kubernetes/Containers/API_Server/CodeStore/installOpenCV4.sh
+++ Kubernetes/Containers/API_Server/CodeStore/installOpenCV4.sh
@@ -2,11 +2,7 @@
 
 #Add swap space for virtual memory ext.
 
-fallocate -l 4.0G /swapfile 
-chmod 600 /swapfile 
-mkswap /swapfile 
-swapon /swapfile
-echo >> /swapfile none swap 0 0
+
 
 #Purge Open CV Old
 
