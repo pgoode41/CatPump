d3f9ef78802e14c0491451edb77902661292e6be
pgoode41 <pgoode41@gmail.com>
2019-11-17 19:12:02 -0600

    Update

--- Kubernetes/Containers/API_Server/CodeStore/installOpenCV4.sh
+++ Kubernetes/Containers/API_Server/CodeStore/installOpenCV4.sh
@@ -2,26 +2,26 @@
 
 #Add swap space for virtual memory ext.
 
-sudo fallocate -l 4.0G /swapfile 
-sudo chmod 600 /swapfile 
-sudo mkswap /swapfile 
-sudo swapon /swapfile
+fallocate -l 4.0G /swapfile 
+chmod 600 /swapfile 
+mkswap /swapfile 
+swapon /swapfile
 echo >> /swapfile none swap 0 0
 
 #Purge Open CV Old
 
-sudo apt-get purge libopencv* -y
+apt-get purge libopencv* -y
 
 
-sudo apt-get update -y
-sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy python3-numpy
-sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
-sudo apt-get install -y curl
-sudo apt install -y python3-pip
-sudo apt-get update -y
+apt-get update -y
+apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy python3-numpy
+apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
+apt-get install -y curl
+apt install -y python3-pip
+apt-get update -y
 
 wget https://github.com/opencv/opencv/archive/4.1.0.zip -O opencv-4.1.0.zip
 wget https://github.com/opencv/opencv_contrib/archive/4.1.0.zip -O opencv-contrib-4.1.0.zip
@@ -33,9 +33,9 @@ mkdir release
 cd release/
 cmake -D WITH_CUDA=ON -D CUDA_ARCH_BIN="5.3" -D CUDA_ARCH_PTX="" -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.1.0/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j3
-sudo make install
-sudo apt-get install -y python-opencv python3-opencv
+make install
+apt-get install -y python-opencv python3-opencv
 
-sudo apt-get install -y libjpeg-dev 
+apt-get install -y libjpeg-dev 
 
-pip3 install -y --user pillow
+pip3 install --user pillow
