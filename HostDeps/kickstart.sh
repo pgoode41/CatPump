e435bcddb036c1c5bf8281b0385bb81f7eba50ac
pgoode41 <pgoode41@gmail.com>
2019-11-30 03:13:03 -0600

    Updates

--- HostDeps/kickstart.sh
+++ HostDeps/kickstart.sh
@@ -12,7 +12,9 @@ pip3 install Jetson.GPIO
 chmod +x "${depsDir}/jetsonSDRezise.sh"
 chmod +x "${depsDir}/K3s.sh"
 chmod +x "${depsDir}/swapspace.sh"
+chmod +x "${depsDir}/installOpenCV4.sh"
 
 "${depsDir}/jetsonSDRezise.sh"
 "${depsDir}/K3s.sh"
 "${depsDir}/swapspace.sh"
+"${depsDir}/installOpenCV4.sh"
