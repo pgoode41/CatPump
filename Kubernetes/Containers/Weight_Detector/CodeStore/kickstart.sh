b4ef491f94331e17377791990fdaf983971d7b3b
pgoode <pgoode41@gmail.com>
2020-02-09 01:20:14 -0600

    Updates

--- Kubernetes/Containers/Weight_Detector/CodeStore/kickstart.sh
+++ Kubernetes/Containers/Weight_Detector/CodeStore/kickstart.sh
@@ -2,4 +2,14 @@
 
 cd ${CATPUMPDIR}
 
+wget https://raw.githubusercontent.com/tbird20d/grabserial/master/grabserial 
+
+sed -i 's#sd.port = ""#sd.port = "/dev/ttyACM0"#g' grabserial
+sed -i 's/sd.baudrate = 115200/sd.baudrate = 9600/g' grabserial
+sed -i 's/except getopt.GetoptError, err:/except getopt.GetoptError:/g' grabserial
+
+chmod +x ./grabserial
+
+#python3 grabserial
+
 python3 apiServer.py
