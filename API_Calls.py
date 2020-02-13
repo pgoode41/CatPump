0cd365c1ee5753130962ff1076c4308f3516a3a8
pgoode <pgoode41@gmail.com>
2020-02-13 01:51:48 -0600

    updates

---
+++ API_Calls.py
@@ -0,0 +1,43 @@
+import os 
+import sys
+import requests
+from pprint import pprint
+import json
+###########################################################################
+###########################################################################
+class CatPump_Api:
+    def __init__(self):
+        print("in init")
+    def StartPump():
+        print("Calling to CatPump_Ai-Api Micro-Service")
+        url = "http://192.168.1.46"
+        port = ":8085"
+        endpoint = "/api/v1/pump/start/"
+        data = {
+            "gpio_pin_number": 21,
+            "pump_run_duration": 5
+        }
+        req = requests.request("POST", url+port+endpoint, data=data)
+        print(req.text)
+###########################################################################
+###########################################################################
+class CatPump_MotionSensor:
+
+    def MotionData(self):
+        print("Calling to CatPump_MotionSensor Micro-Service")
+###########################################################################
+###########################################################################
+class CatPump_WeightSensor:
+
+    def GetWeight(self):
+        print("Calling to CatPump_WeightSensor Micro-Service")
+###########################################################################
+###########################################################################
+class CatPump_Updater:
+
+    def ApplyUpdate(self):
+        print("Calling to CatPump_Updater Micro-Service")
+###########################################################################
+###########################################################################
+
+CatPump_Api.StartPump()
--- Kubernetes/Containers/Ai_Detector/CodeStore/Motion_Sensor/__init__.py
+++ Kubernetes/Containers/Ai_Detector/CodeStore/Motion_Sensor/__init__.py
@@ -9,26 +9,31 @@ PIR_PIN = 31
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 def MOTION(PIR_PIN):
-                print("Motion Detected!")
-                url = "http://192.168.1.46"
-                port = ":8085"
-                endpoint = '/api/v1/detect/'
-                #reqDict = request.form.to_dict()
-                #pprint(reqDict)
-                req = requests.request("GET", url+port+endpoint)
-                print(req.text)
-                print("Facial Detection De-activated!!!")
+    print("Motion Detected!")
+    try:
+        url = "http://192.168.1.46"
+        port = ":8085"
+        endpoint = '/api/v1/detect/'
+        #reqDict = request.form.to_dict()
+        #pprint(reqDict)
+        req = requests.request("GET", url+port+endpoint)
+        print(req.text)
+        print("Facial Detection De-activated!!!")
+    except:
+        print("Error sending data to catpump-api detection endpoint")
+
 
 print ("PIR Module Test (CTRL+C to exit)")
 time.sleep(2)
 print ("Ready")
 
 try:
-               GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
-               while 1:
-                              time.sleep(100)
+    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
+    while 1:
+        time.sleep(100)
 except KeyboardInterrupt:
-               print("Quit")
-               GPIO.cleanup()
+    print("Quit")
+    GPIO.cleanup()
+
 
 #python3 /opt/CatPump/Motion_Sensor/__init__.py
